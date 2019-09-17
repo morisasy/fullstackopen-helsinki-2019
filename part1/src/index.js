304e69e7e469510e4d8802e7c7c4f4d3168d47a6
Medi Risasi <morisasy@gmail.com>
2019-09-17 21:49:31 +0300

    course information part 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,16 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
+      <h1>{course}</h1>
       <p>
-        {a} plus {b} is {a + b}
+        {part1} {exercises1}
       </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
