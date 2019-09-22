5b35252c2e668fc28c7e5fe2919e916f104ec4bf
Medi Risasi <morisasy@gmail.com>
2019-09-22 14:46:16 +0300

    History commponent

--- parts/counterapp/src/index.js
+++ parts/counterapp/src/index.js
@@ -1,5 +1,23 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = (props) => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -17,14 +35,14 @@ const App = (props) => {
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-        <p>{allClicks.join(' ')}</p>
-      </div>
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
     </div>
+  </div>
   )
 }
 
